62c5b0f7cc588a69be8ded868c8fca4163513ee4
Nigel D'Souza <nigel@brandmovers.in>
2015-10-22 17:17:42 -0500

    Initial commit

---
+++ _site/js/terminal.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function($) {
+    $('#terminal').terminal({
+            help: function () {
+                this.echo('');
+                this.echo('[[;#E2462A;#000]profiles]         all my social profiles');
+                this.echo('[[;#D4E22A;#000]languages]        programming languages of course');
+                this.echo('[[;#2AE27F;#000]softwares]        software I use');
+                this.echo('[[;#2ACEE2;#000]pgp]              send me something encrypted');
+                this.echo('');
+                this.echo('[[;#E22AD1;#000]clear]          clears the console');
+            },
+            profiles: function() {
+                this.echo('');
+                this.echo("Github           http://github.com/xcrucifier");
+                this.echo("Twitter          http://twitter.com/nigelmd");
+                this.echo("LinkedIn         http://linkedin.com/in/nigelmd");
+                this.echo("Keybase          http://keybase.io/xcrucifier");
+                this.echo('');
+            },
+            languages: function() {
+                this.echo('');
+                this.echo('Python');
+                this.echo('Java');
+                this.echo('Scala');
+                this.echo('C/C++');
+                this.echo('PHP');
+                this.echo('HTML5');
+                this.echo('JavaScript');
+                this.echo('');
+            },
+            softwares: function() {
+                this.echo('');
+                this.echo('Mac OSX');
+                this.echo('Z shell');
+                this.echo('Windows');
+                this.echo('Linux(Ubuntu, Redhat)');
+                this.echo('All Jetbrains products');
+                this.echo('Sequel Pro');
+                this.echo('Homebrew');
+                this.echo('Vagrant');
+                this.echo('Amazon Web Services');
+                this.echo('');
+            },
+            pgp: function() {
+                this.echo('');
+                this.echo('PGP              http://pgp.mit.edu/pks/lookup?search=0x6C8571E9');
+                this.echo('Fingerprint      9B87 856A 6C85 71E9');
+                this.echo('');
+            }
+        },
+        {
+            prompt: 'guest@nigeldsouza> ',
+            name: 'Nigel\'s Terminal',
+            height: 400,
+            enabled: true,
+            greetings: 'Hi, welcome to my website. Type [[;#2AE24C;#000]help] for more options',
+            history: true,
+            cookie: true,
+            exit: true,
+            keypress: function(e) {
+                if (e.which == 96) {
+                    return false;
+                }
+            },
+            tabcomplete: function() { return ""; }
+        });
+});
---
+++ about.md
@@ -0,0 +1,12 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+Hi, I'm [Nigel D'Souza](http://nigeldsouza.io), a developer and tech enthusiast, currently pursuing my Masters in Computer Science at DePaul University.
+I love learning new concepts and technologies and meeting new people. In my free time I enjoy playing basketball, video games, dancing, skating and drumming.
+
+You can tweet me on Twitter at [@nigelmd](http://twitter.com/nigelmd), view my [Github](http://github.com/xcrucifier) page, contact me on [LinkedIn](http://linkedin.com/in/nigelmd) or just [email me](mailto:nigeldsouza@outlook.com).
+
+My [KeyId](http://pgp.mit.edu/pks/lookup?search=0x6C8571E9) and Fingerprint [9B87 856A 6C85 71E9](https://keybase.io/xcrucifier)
